299b6d0971e0d051d43d82c1d764b55acbdf974d
Raju Gautam <Rajugautam45@gmail.com>
2018-09-25 00:33:53 +0530

    contains components and containers demo

---
+++ src/Components/DemoComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import classes from './DemoComponent.scss';
+
+export default  DemoComponent = () => {
+    return <div className={classes.demo}>Hello React!</div>;
+  };
+  
---
+++ src/Containers/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from './App.scss';
+
+
+export default  App = () => {
+    return <div className={classes.ah}>Hello React!</div>;
+  };
+  
