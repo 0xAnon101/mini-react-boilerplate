539cfdb8327627018a27a42ecb4a1ed9f7c45eca
Raju Gautam <rajugautam45@gmail.com>
2019-04-26 14:45:26 +0530

    added support for image/svg/gif files

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,62 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin =  require('mini-css-extract-plugin');
-
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Path = require("path");
 
 const miniCssPlugin = new MiniCssExtractPlugin({
-    filename: '[name].[hash].css',
-    chunkFilename:  '[id].[hash].css',
-})
+  filename: "[name].[hash].css",
+  chunkFilename: "[id].[hash].css"
+});
 
-const htmlPlugin =  new HtmlWebPackPlugin({
-    template: "./src/index.html",
-    filename: "./index.html"
-})
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
 
-module.exports  = (env,argv) => {
-
-    return {
+module.exports = (env, argv) => {
+  return {
     optimization: {
-        nodeEnv: argv.mode
+      nodeEnv: argv.mode
     },
     module: {
-        rules: [
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            argv.mode == "development"
+              ? "style-loader"
+              : MiniCssExtractPlugin.loader,
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader:  'babel-loader'
-                }
+              loader: "css-loader",
+              options: {
+                importLoaders: 1
+              }
             },
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+          include: [Path.join(__dirname, "src/assets/")],
+          loader: "file-loader?name=assets/[name].[ext]"
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
             {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                  argv.mode =='development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                  {
-                    loader: 'css-loader',
-                    options: {
-                    modules: true,
-                    importLoaders: 1,
-                    localIdentName: "[name]_[local]_[hash:base64:5]",
-                    }
-                  },
-                  'sass-loader'
-                ]
+              loader: "file-loader",
+              options: {}
             }
-           
-        ]
+          ]
+        }
+      ]
     },
-    plugins: [htmlPlugin,miniCssPlugin]
-}
-}
+    plugins: [htmlPlugin, miniCssPlugin]
+  };
+};
